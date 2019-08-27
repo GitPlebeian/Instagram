e959f86fa8e5476a4d58afe88908b36bcbdc1790
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-27 13:52:26 -0600

    Initial Commit

---
+++ PhotoBoi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoBoi
+//
+//  Created by Jackson Tubbs on 8/27/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
